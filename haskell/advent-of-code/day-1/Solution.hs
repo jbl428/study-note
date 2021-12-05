1a565e3de2fe6442328883f3058047ccdd2f2046
Jake Son <sonjeabin@gmail.com>
2021-12-05 11:00:17 +0900

    Update solution of day 1

--- haskell/advent-of-code/day-1/Solution.hs
+++ haskell/advent-of-code/day-1/Solution.hs
@@ -1,14 +1,15 @@
 countIncreases :: Int -> [Int] -> Int
-countIncreases count [] = count
-countIncreases count [x] = count
-countIncreases count (x:y:zx)
-    | x < y = countIncreases (count + 1) (y:zx)
-    | otherwise = countIncreases count (y:zx)
+countIncreases count (x : y : zx)
+  | x < y = countIncreases (count + 1) (y : zx)
+  | otherwise = countIncreases count (y : zx)
+countIncreases count _ = count
 
 calc :: String -> Int
-calc x = countIncreases 0 $ read <$> lines x
+calc x = countIncreases 0 $ threeWindowsSum $ read <$> lines x
+
+threeWindowsSum :: [Int] -> [Int]
+threeWindowsSum (x : y : z : zs) = (x + y + z) : threeWindowsSum (y : z : zs)
+threeWindowsSum _ = []
 
 main :: IO ()
-main = do
-  content <- readFile "input.txt"
-  print $ calc content
+main = readFile "input.txt" >>= print . calc
