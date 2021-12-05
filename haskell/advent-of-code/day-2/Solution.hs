27caf02d83b67542017b3f894c7d835bd88dc8fe
Jake Son <sonjeabin@gmail.com>
2021-12-05 11:17:36 +0900

    Update solution of day 2

--- haskell/advent-of-code/day-2/Solution.hs
+++ haskell/advent-of-code/day-2/Solution.hs
@@ -3,25 +3,25 @@ import Data.Maybe (mapMaybe)
 data Mode = Forward | Up | Down deriving (Show)
 
 toInputs :: [String] -> Maybe (Mode, Int)
-toInputs [mode, amount] = case mode of
+toInputs [mode, unit] = case mode of
   "forward" -> Just (Forward, value)
   "up" -> Just (Up, value)
   "down" -> Just (Down, value)
   _ -> Nothing
   where
-    value = read amount :: Int
+    value = read unit :: Int
 toInputs _ = Nothing
 
-calc :: [(Mode, Int)] -> (Int, Int)
-calc = foldl temp (0, 0)
+calc :: [(Mode, Int)] -> (Int, Int, Int)
+calc = foldl temp (0, 0, 0)
   where
-    temp (h, d) (m, a) = case m of
-      Forward -> (h + a, d)
-      Up -> (h, d - a)
-      Down -> (h, d + a)
+    temp (horizontal, depth, aim) (mode, unit) = case mode of
+      Forward -> (horizontal + unit, depth + aim * unit, aim)
+      Up -> (horizontal, depth, aim - unit)
+      Down -> (horizontal, depth, aim + unit)
 
 solution :: String -> Int
-solution = uncurry (*) . calc . mapMaybe (toInputs . words) . lines
+solution = (\(a, b, _) -> a * b) . calc . mapMaybe (toInputs . words) . lines
 
 main :: IO ()
 main = readFile "input.txt" >>= print . solution
