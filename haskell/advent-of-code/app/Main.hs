2936ffc632942db8cd916b64054e0cace519cbfc
Jake Son <sonjeabin@gmail.com>
2021-12-26 19:27:29 +0900

    Refactoring codes

--- haskell/advent-of-code/app/Main.hs
+++ haskell/advent-of-code/app/Main.hs
@@ -1,6 +1,11 @@
 module Main where
 
-import Lib
+import qualified Day1.Solution as D1
+import qualified Day2.Solution as D2
 
 main :: IO ()
-main = someFunc
+main = do
+  content <- readFile "input/day1.txt"
+  print $ D1.solve content
+  content <- readFile "input/day2.txt"
+  print $ D2.solve content
--- haskell/advent-of-code/test/Spec.hs
+++ haskell/advent-of-code/test/Spec.hs
@@ -5,9 +5,11 @@ import Test.Hspec (describe, hspec, it, shouldBe)
 main :: IO ()
 main = hspec $ do
   describe "Day 1" $ do
+    let input = unlines ["199", "200", "208", "210", "200", "207", "240", "269", "260", "263"]
     it "should success" $ do
-      D1.solve "123\n234\n121\n323" `shouldBe` 1
+      D1.solve input `shouldBe` 5
 
   describe "Day 2" $ do
+    let input = unlines ["forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"]
     it "should success" $ do
-      D2.solve "123\n234\n121\n323" `shouldBe` 0
+      D2.solve input `shouldBe` 900
