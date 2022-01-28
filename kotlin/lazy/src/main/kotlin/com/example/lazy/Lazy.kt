981ecca50e7abeb498e2e8fbe339e9f99901f02c
Jake Son <sonjeabin@gmail.com>
2022-01-28 22:46:45 +0900

    Add flatMap and map2 of lazy monad

--- kotlin/lazy/src/main/kotlin/com/example/lazy/Lazy.kt
+++ kotlin/lazy/src/main/kotlin/com/example/lazy/Lazy.kt
@@ -10,3 +10,9 @@ class Lazy<out A>(fn: () -> A) {
 fun or(a: Lazy<Boolean>, b: Lazy<Boolean>) = a() || b()
 
 fun <A, B> map(fn: (A) -> B, fa: Lazy<A>): Lazy<B> = Lazy { fn(fa()) }
+
+fun <A, B> flatMap(fn: (A) -> Lazy<B>, fa: Lazy<A>): Lazy<B> = Lazy { map(fn, fa)()() }
+
+fun <A, B, C> map2(fn: (A, B) -> C, fa: Lazy<A>, fb: Lazy<B>): Lazy<C> = Lazy {
+    fn(fa(), fb())
+}
--- kotlin/lazy/src/test/kotlin/com/example/lazy/LazyTest.kt
+++ kotlin/lazy/src/test/kotlin/com/example/lazy/LazyTest.kt
@@ -41,4 +41,45 @@ internal class LazyTest {
         assertEquals(result(), 20)
         assertEquals(count, 1)
     }
+
+    @Test
+    fun `FlatMap 동작 테스트`() {
+        // given
+        var count = 0
+        val lazy = Lazy {
+            count++
+            10
+        }
+
+        // when
+        val result = flatMap({ a -> Lazy { a + 10 } }, lazy)
+
+        // then
+        assertEquals(count, 0)
+        assertEquals(result(), 20)
+        assertEquals(count, 1)
+    }
+
+    @Test
+    fun `Map2 동작 테스트`() {
+        // given
+        var count = 0
+        val lazyA = Lazy {
+            count++
+            10
+        }
+        val lazyB = Lazy {
+            count++
+            20
+        }
+        fun add(a: Int, b: Int): Int = a + b
+
+        // when
+        val result = map2(::add, lazyA, lazyB)
+
+        // then
+        assertEquals(count, 0)
+        assertEquals(result(), 30)
+        assertEquals(count, 2)
+    }
 }
