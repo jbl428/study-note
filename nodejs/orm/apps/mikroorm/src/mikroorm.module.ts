ebab598a40684c47fa6d3b9a7d39eed916d903db
Jake Son <sonjeabin@gmail.com>
2022-02-19 11:33:05 +0900

    [nodejs/orm] Add orm packages

--- nodejs/orm/apps/mikroorm/src/mikroorm.module.ts
+++ nodejs/orm/apps/mikroorm/src/mikroorm.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MikroormController } from './mikroorm.controller';
 import { MikroormService } from './mikroorm.service';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 
 @Module({
-  imports: [],
+  imports: [
+    MikroOrmModule.forRoot({
+      type: 'postgresql',
+      user: 'test',
+      password: 'test',
+      dbName: 'test',
+    }),
+  ],
   controllers: [MikroormController],
   providers: [MikroormService],
 })
--- nodejs/orm/apps/typeorm/src/typeorm.module.ts
+++ nodejs/orm/apps/typeorm/src/typeorm.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeormController } from './typeorm.controller';
 import { TypeormService } from './typeorm.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      username: 'test',
+      password: 'test',
+      database: 'test',
+    }),
+  ],
   controllers: [TypeormController],
   providers: [TypeormService],
 })
