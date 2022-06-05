d35ff644b570537ad710b7ce4a6bc43eca099a7a
Jake Son <sonjeabin@gmail.com>
2022-06-05 16:11:51 +0900

    [spring-graphql] add auth web filter

---
+++ kotlin/spring-graphql/src/main/kotlin/com/graphql/sample/config/AuthenticationManager.kt
@@ -0,0 +1,15 @@
+package com.graphql.sample.config
+
+import org.springframework.security.authentication.ReactiveAuthenticationManager
+import org.springframework.security.core.Authentication
+import org.springframework.stereotype.Component
+import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toMono
+
+@Component
+class AuthenticationManager : ReactiveAuthenticationManager {
+
+    override fun authenticate(authentication: Authentication): Mono<Authentication> {
+        return authentication.toMono()
+    }
+}
--- kotlin/spring-graphql/src/main/kotlin/com/graphql/sample/config/SecurityConfig.kt
+++ kotlin/spring-graphql/src/main/kotlin/com/graphql/sample/config/SecurityConfig.kt
@@ -2,14 +2,17 @@ package com.graphql.sample.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.ReactiveAuthenticationManager
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.config.web.server.invoke
-import org.springframework.security.core.userdetails.MapReactiveUserDetailsService
-import org.springframework.security.core.userdetails.User
-import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.security.web.server.authentication.AuthenticationWebFilter
+import reactor.kotlin.core.publisher.toMono
 
 @Configuration
 @EnableWebFluxSecurity
@@ -17,20 +20,30 @@ import org.springframework.security.web.server.SecurityWebFilterChain
 class SecurityConfig {
 
     @Bean
-    fun springWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain =
+    fun springWebFilterChain(
+        http: ServerHttpSecurity,
+        manager: AuthenticationManager,
+    ): SecurityWebFilterChain =
         http {
             csrf { disable() }
             authorizeExchange {
-                authorize(anyExchange, permitAll)
+                authorize(anyExchange, authenticated)
             }
-            httpBasic {}
+            httpBasic { disable() }
+            addFilterAt(jwtAuthenticationFilter(manager), SecurityWebFiltersOrder.AUTHENTICATION)
         }
 
-    @Bean
-    fun userDetailsService(): MapReactiveUserDetailsService {
-        val userBuilder = User.withDefaultPasswordEncoder()
-        val rob: UserDetails = userBuilder.username("rob").password("rob").roles("USER").build()
-        val admin: UserDetails = userBuilder.username("admin").password("admin").roles("USER", "ADMIN").build()
-        return MapReactiveUserDetailsService(rob, admin)
+    fun jwtAuthenticationFilter(manager: ReactiveAuthenticationManager): AuthenticationWebFilter {
+        val bearerAuthenticationFilter = AuthenticationWebFilter(manager)
+
+        bearerAuthenticationFilter.setServerAuthenticationConverter {
+            UsernamePasswordAuthenticationToken(
+                "",
+                "",
+                listOf(SimpleGrantedAuthority("ROLE_ADMIN"))
+            ).toMono()
+        }
+
+        return bearerAuthenticationFilter
     }
 }
