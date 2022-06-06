702df8d39129036b515492dfeeb3bb315f924809
Jake Son <sonjeabin@gmail.com>
2022-06-06 13:03:20 +0900

    [spring-graphql] add ktlint package

--- kotlin/spring-graphql/build.gradle.kts
+++ kotlin/spring-graphql/build.gradle.kts
@@ -3,6 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.7.0"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
+
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
 }
