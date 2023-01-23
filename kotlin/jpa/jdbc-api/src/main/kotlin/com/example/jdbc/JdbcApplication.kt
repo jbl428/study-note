c6c57e66d683016a98af0769d8f76ae6202d747a
Jake Son <sonjeabin@gmail.com>
2023-01-23 12:10:28 +0900

    [kotlin-jpa] add jdbc auditing

--- kotlin/jpa/jdbc-api/src/main/kotlin/com/example/jdbc/JdbcApplication.kt
+++ kotlin/jpa/jdbc-api/src/main/kotlin/com/example/jdbc/JdbcApplication.kt
@@ -2,7 +2,9 @@ package com.example.jdbc
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing
 
+@EnableJdbcAuditing
 @SpringBootApplication
 class JdbcApplication
 
--- kotlin/jpa/jdbc-entity/src/main/kotlin/com/example/entity/BaseEntity.kt
+++ kotlin/jpa/jdbc-entity/src/main/kotlin/com/example/entity/BaseEntity.kt
@@ -1,11 +1,22 @@
 package com.example.entity
 
+import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.Id
+import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.domain.Persistable
+import java.time.LocalDateTime
 
 open class BaseEntity : Persistable<Long> {
     @Id
     var id: Long = 0L
+        protected set
+
+    @CreatedDate
+    lateinit var createdAt: LocalDateTime
+        protected set
+
+    @LastModifiedDate
+    lateinit var updatedAt: LocalDateTime
         protected set
 
     override fun getId(): Long = id
